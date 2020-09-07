e94271b2754613600b3470c8ff7dfb42eb391085
roman <romanshashkin@mail.ru>
2020-09-07 17:35:41 +0400

    "add db for server"

--- cian_parser/urls.py
+++ cian_parser/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from cian_parser import views
@@ -10,5 +11,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')
+         ),
+    path('admin/', admin.site.urls)
 ]
