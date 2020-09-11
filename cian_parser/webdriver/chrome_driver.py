eeefdfffb310466d4d07ae91ef103ef136342e39
roman <romanshashkin@mail.ru>
2020-09-11 14:32:09 +0400

    "add test chromedriver vpn"

--- cian_parser/webdriver/chrome_driver.py
+++ cian_parser/webdriver/chrome_driver.py
@@ -20,13 +20,16 @@ path = (os.getenv('chrome_profile_one'),
 class Chromedriver():
 
     def start_driver(self, path=str(bd) + os.getenv('chrome_driver_path'),
-                     chrome_profile= str(bd) + os.getenv('chrome_profile_one')):
+                     chrome_profile=str(bd) + os.getenv('chrome_profile_one')):
         options = Options()
         prefs = {"profile.managed_default_content_settings.images": 2}
         options.binary_location = '/opt/google/chrome/google-chrome'
         options.add_experimental_option("prefs", prefs)
         options.add_argument('--headless')
         options.add_argument('-no-sandbox')
+        # path_extension = '/home/roman/PycharmProjects/cian_parser/cian_parser/webdriver/chrome_driver/chrome_profile/google-chrome/Default/Extensions/hipncndjamdcmphkgngojegjblibadbe/1.6.1_0.crx'
+        # options.add_extension(str(bd) + '/cian_parser/webdriver/chrome_driver/chrome_profile/google-chrome/Default/Extensions/bibjcjfmgapbfoljiojpipaooddpkpai/1.5_0.crx')
+        # options.add_argument("load-extension=" + path_extension)
         options.add_argument('user-data-dir=' + chrome_profile)
         self.driver = webdriver.Chrome(executable_path=path, options=options)
         return self.driver
@@ -38,4 +41,6 @@ class Chromedriver():
 # driver.get(url)
 # print(driver.find_element_by_class_name('ip').text)
 # # driver.quit()
-# '94.229.237.244'
+# '91.210.171.227'
+# # '94.229.237.244'
+# '178.62.106.169'
