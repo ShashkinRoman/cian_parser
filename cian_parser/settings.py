f25aeadd9aeac5d8a7f2d293cb7d344e070629aa
roman <romanshashkin@mail.ru>
2020-11-30 21:53:53 +0400

    correct urls_photo

--- cian_parser/settings.py
+++ cian_parser/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/uploads')
-MEDIA_URL = '/media/uploads/'
+MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
