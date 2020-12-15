ec95f4e23cd402e8635afadb8438911bbbc82ca1
roman <romanshashkin@mail.ru>
2020-12-15 23:28:58 +0400

    correct func image

--- cian_parser/serializers.py
+++ cian_parser/serializers.py
@@ -2,8 +2,7 @@ import json
 from django.contrib.auth.models import User, Group
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers, filters
-from cian_parser.models import InformationFromAds, UrlsAds, SerializerInfo
-    # CianPhoto
+from cian_parser.models import InformationFromAds, UrlsAds, SerializerInfo, CianPhoto
 
 
 class InformationFromAdsSerializer(serializers.ModelSerializer):
@@ -25,17 +24,21 @@ class SerializerInfoSerializer(serializers.ModelSerializer):
     is_client = serializers.ReadOnlyField()
     images = serializers.SerializerMethodField()
 
-
     def get_images(self, obj):
-        images_json_serialized = obj.ser_url_ads.url_ads.urls_on_photo
+        obj_cian_photo = obj.images.all()
         list_of_dict_images = []
-        try:
-            images_json = json.loads(images_json_serialized)
-        except json.decoder.JSONDecodeError:
-            return list_of_dict_images
-        for image in images_json:
-            list_of_dict_images.append({'image': image.replace('-2.jpg', '-1.jpg')})
+        for image in obj_cian_photo:
+            list_of_dict_images.append({'image': image.image})
         return list_of_dict_images
+        # images_json_serialized = obj.ser_url_ads.url_ads.urls_on_photo
+        # list_of_dict_images = []
+        # try:
+        #     images_json = json.loads(images_json_serialized)
+        # except json.decoder.JSONDecodeError:
+        #     return list_of_dict_images
+        # for image in images_json:
+        #     list_of_dict_images.append({'image': image.replace('-2.jpg', '-1.jpg')})
+        # return list_of_dict_images
 
     class Meta:
         model = SerializerInfo
